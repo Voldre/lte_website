a62ecc975df4c4718a0e68df2af3bd2e30ad9b8e
voldre <v.dre@laposte.net>
2020-05-02 16:21:25 +0200

    Ajout d'une liste de page sur le Forum

--- page_forum_index.php
+++ page_forum_index.php
@@ -10,15 +10,73 @@ include("menu.php");
 
 <body style="background-image:none;background-color:black;">
 
+
+
+
 <section class="first">
+<?php  // Afficher des pages pour naviguer entre les articles : 5 articles par page
 
+// Compter le nombre d'articles
 
+$reponse_articles = $bdd ->query('SELECT COUNT(ID) AS nb_articles FROM articles');
+
+$nb_articles = 0 ;
+
+while ($donnees_articles = $reponse_articles->fetch())
+{
+    $nb_articles = $donnees_articles['nb_articles']; // On stock le nb d'articles
+}
+$reponse_articles->closeCursor();
+
+$nb_pages = floor( 1 + $nb_articles / 5);
+// floor permet de faire un arrondis inférieur, donc si on a 2,6 pages, on a 2 pages
+// Le "+1" permet d'en placer une obligatoire, et ensuite, si le nb_articles est > a 5, on en rajoute 1, >10 on en rajoute 2, etc...
+
+echo "<p>Nombres de Pages: ",$nb_pages, "<br/>Nombres d'articles: ", $nb_articles,"</p>";
+
+echo "<span class=\"page_nav\">";
+for ($page = 1 ; $page <= $nb_pages ; $page++) 
+{
+    ?>
+<a href=<?php echo "\"page_forum_index.php?page=",$page,"\""?>><?php echo $page;?></a>
+<?php
+}
+?>
+
+</span> <br /><br />
 <?php
   //  $reponse = $bdd ->query('SELECT * FROM articles ORDER BY date_creation DESC LIMIT 5');    
     
   // Passons par les jointures pour récupérer la correspondance PSEUDO = Nom d'auteur quand membres.ID = articles.ID_auteur
 
-    $reponse = $bdd ->query('SELECT *, articles.ID AS ID_article FROM articles INNER JOIN membres ON membres.ID = articles.ID_auteur ORDER BY date_creation DESC LIMIT 8');
+  if (isset($_GET['page']))
+  {
+    $_GET['page'] = (int) htmlspecialchars($_GET['page']);
+
+    $numero_article = 5 * ($_GET['page']-1);
+    // Valeur du premier numéro d'article selon la page 
+
+        //Si valeur fausse, on met à la page 1
+        if ($numero_article < 0)
+        {
+            $numero_article = 0;
+            $_GET['page'] = 1;
+        }
+
+        $reponse = $bdd ->query('SELECT *, articles.ID AS ID_article FROM articles INNER JOIN membres ON membres.ID = articles.ID_auteur 
+        ORDER BY date_creation DESC LIMIT ' . $numero_article . ',5');
+        
+        // IL EST HYPER IMPORTANT DE METTRE DES "." dans le code SQL pour concaténer des éléments!
+        // Contrairement aux fonctions PHP, la virgule ne fonctionne pas ici!
+        // En SQL, la seule concaténation autorisé c'est le "."
+        
+        // Donc vraiment, faut prendre l'habitude de concaténer par des "." et pas des ","
+
+ }
+else
+{
+     $reponse = $bdd ->query('SELECT *, articles.ID AS ID_article FROM articles INNER JOIN membres ON membres.ID = articles.ID_auteur ORDER BY date_creation DESC LIMIT 0,5');
+}
 
 while ($donnees = $reponse->fetch()) 
 { 
